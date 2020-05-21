147a8d6af8de0404c9e9e48534c0214ea16623a1
Lakshanikodikara <61499073+Lakshanikodikara@users.noreply.github.com>
2020-05-21 17:43:24 +0530

    done labsheet 5

---
+++ app/src/main/java/com/example/labsheet5/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.labsheet5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button startBtn;
+    TextView txt;
+    public static final String BROADCAST_ACTION = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startBtn = findViewById(R.id.btnStartBroadcast);
+        txt = findViewById(R.id.txtViewMsg);
+    }
+    protected void onStart() {
+        super.onStart();
+        Reciever localLisetner = new Reciever();
+        IntentFilter intentFilter=new IntentFilter(BROADCAST_ACTION);
+        this.registerReceiver(localLisetner,intentFilter);
+    }
+
+    protected void onStop(){
+        super.onStop();
+        Reciever localLisetner = new Reciever();
+        this.unregisterReceiver(localLisetner);
+    }
+
+    public void onClick(View view){
+        if(view.getId()==R.id.btnStartBroadcast){
+            BackgroundService.startAction(this.getApplicationContext());
+        }
+    }
+
+
+    public class Reciever extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String currentText=txt.getText().toString();
+            String message = intent.getStringExtra("value");
+            currentText += "\nRecieved " +message;
+            txt.setText(currentText);
+        }
+    }
+
+
+}
